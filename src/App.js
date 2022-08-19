ed49ca0072f98f204d0c9105f427711ecc9cec93
esadakman <esadd26@gmail.com>
2022-08-19 11:41:33 +0300

    fetch user info

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, createRef } from "react";
+import { useState, createRef, useEffect } from "react";
 import { useScreenshot } from "use-react-screenshot";
 import "./App.scss";
 import {
@@ -9,6 +9,23 @@ import {
   VerifiedIcon,
 } from "./icons";
 import { AvatarLoader } from "./loader";
+
+function convertImgToBase64(url, callback, outputFormat) {
+  var canvas = document.createElement("CANVAS");
+  var ctx = canvas.getContext("2d");
+  var img = new Image();
+  img.crossOrigin = "Anonymous";
+  img.onload = function () {
+    canvas.height = img.height;
+    canvas.width = img.width;
+    ctx.drawImage(img, 0, 0);
+    var dataURL = canvas.toDataURL(outputFormat || "image/png");
+    callback.call(this, dataURL);
+    // Clean up
+    canvas = null;
+  };
+  img.src = url;
+}
 
 const tweetFormat = (tweet) => {
   tweet = tweet
@@ -31,6 +48,7 @@ const formatNumber = (number) => {
 
 function App() {
   const tweetRef = createRef(null);
+  const downloadRef = createRef(null);
   const [name, setName] = useState();
   const [userName, setUserName] = useState();
   const [isVerified, setIsVerified] = useState(false);
@@ -40,6 +58,15 @@ function App() {
   const [quoteTweets, setQuoteTweets] = useState(0);
   const [likes, setLikes] = useState(0);
   const [image, takeScreenshot] = useScreenshot();
+  const [langText, setLangText] = useState();
+
+  const getImage = () => takeScreenshot(tweetRef.current);
+
+  useEffect(() => {
+    if (image) {
+      downloadRef.current.click();
+    }
+  }, [image]);
 
   const avatarHandle = (e) => {
     const file = e.target.files[0];
@@ -48,6 +75,29 @@ function App() {
       setAvatar(this.result);
     });
     reader.readAsDataURL(file);
+  };
+
+  const fetchTwitterInfo = () => {
+    fetch(
+      `https://typeahead-js-twitter-api-proxy.herokuapp.com/demo/search?q=${userName}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const twitter = data[0];
+        console.log(twitter);
+
+        convertImgToBase64(
+          twitter.profile_image_url_https,
+          function (base64Image) {
+            setAvatar(base64Image);
+          }
+        );
+        setName(twitter.name);
+        setUserName(twitter.screen_name);
+        setTweet(twitter.status.text);
+        setRetweets(twitter.status.retweet_count);
+        setLikes(twitter.status.favorite_count);
+      });
   };
 
   return (
@@ -120,10 +170,27 @@ function App() {
               onChange={(e) => setLikes(e.target.value)}
             />
           </li>
-          <button>Oluştur</button>
+          <button onClick={getImage}>Oluştur</button>
+          <div className="download-url">
+            {image && (
+              <a ref={downloadRef} href={image} download="tweet.png">
+                Tweeti İndir
+              </a>
+            )}
+          </div>
         </ul>
       </div>
       <div className="tweet-container">
+        <div className="fetch-info">
+          <input
+            type="text"
+            value={userName || ""}
+            placeholder="Twitter kullanıcı adını yazın"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <button onClick={fetchTwitterInfo}>Bilgileri Çek</button>
+        </div>
+
         <div className="tweet" ref={tweetRef}>
           <div className="tweet-author">
             {(avatar && <img src={avatar} alt="avatar" />) || <AvatarLoader />}
